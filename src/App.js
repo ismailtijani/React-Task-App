316cc39a8a94a0e20948fc49dd9ef3c5b2335c0b
Ismail Tijani <ismailtijani10@yahoo.com>
2023-03-29 23:56:31 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.scss";
+import { BsPlusCircleFill, BsCircle } from "react-icons/bs";
+import { MdOutlineCalendarToday } from "react-icons/md";
+import { BiChevronDown } from "react-icons/bi";
+import axios from "axios";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get("./data.json");
+      setTodos(result.data.todo);
+    };
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="task-container">
+        <div className="task-container-header">
+          <div className="task-container-heading">
+            <span>
+              <BsPlusCircleFill className="add-icon" />
+            </span>
+            <input type="text" placeholder="Add a task" />
+          </div>
+        </div>
+        <div className="all-tasks">
+          <h2>Task</h2>
+
+          <div className="tasks">
+            {todos.map((todo) => (
+              <div key={todo.id} className="task">
+                <div className="left">
+                  <span>
+                    <BsCircle className="circle-icon" />
+                  </span>
+                  <div className="task-text">
+                    <h4>{todo.title}</h4>
+                    <span>0/1</span> &nbsp;&nbsp;
+                    <span>
+                      <MdOutlineCalendarToday /> Today
+                    </span>
+                  </div>
+                </div>
+                <span>
+                  <BiChevronDown />
+                </span>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
