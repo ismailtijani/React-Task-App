f2db307d7f096c989801fdc951b28668d410ce85
Ismail Tijani <ismailtijani10@yahoo.com>
2023-04-01 04:26:43 +0100

    Implement Tasks, Sub-Tasks, and finished task UI

--- src/App.js
+++ src/App.js
@@ -1,57 +1,228 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.scss";
 import { BsPlusCircleFill, BsCircle } from "react-icons/bs";
-import { MdOutlineCalendarToday } from "react-icons/md";
-import { BiChevronDown } from "react-icons/bi";
-import axios from "axios";
+import {
+  MdOutlineCalendarToday,
+  MdDelete,
+  MdEdit,
+  MdAdd,
+} from "react-icons/md";
+import { BiChevronDown, BiChevronUp } from "react-icons/bi";
+import { AiFillCheckCircle } from "react-icons/ai";
+import data from "./data.json";
 
 function App() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState(data);
+  const [title, setTitle] = useState("");
+  const [edit, setEdit] = useState(false);
+  const [showSubtodo, setShowSubtodo] = useState(false);
+  const [completedTodos, setCompletedTodos] = useState([]);
 
   useEffect(() => {
-    const fetchData = async () => {
-      const result = await axios.get("./data.json");
-      setTodos(result.data.todo);
+    const completedTodo = todos.filter((todo) => todo.completed === true);
+    setCompletedTodos(completedTodo);
+  }, [todos]);
+
+  // console.log(sCompletedTodos);
+
+  const addTodo = (e) => {
+    e.preventDefault();
+
+    if (title === "") {
+      alert("Add a todo");
+      return;
+    }
+
+    const newTodo = {
+      id: todos.length + 1,
+      title,
+      completed: false,
+      subTodos: [],
     };
-    fetchData();
-  }, []);
+
+    setTodos([...todos, newTodo]);
+  };
+
+  const markTodoAsComplete = (id) => {
+    const newTodo = [...todos];
+    newTodo[id - 1].completed = true;
+    setTodos(newTodo);
+  };
+
+  const updateTodo = (id, text) => {
+    const newTodos = [...todos];
+    newTodos[id - 1].title = text;
+    setTodos(newTodos);
+    setEdit(false);
+  };
+
+  const handleEditChange = (id, text) => {
+    setEdit(id);
+    setTitle(text);
+  };
+
+  const removeTodo = (id) => {
+    const newTodos = [...todos];
+    newTodos.splice(id - 1, 1);
+    setTodos(newTodos);
+  };
 
   return (
     <div className="app">
       <div className="task-container">
         <div className="task-container-header">
           <div className="task-container-heading">
-            <span>
+            <span onClick={addTodo}>
               <BsPlusCircleFill className="add-icon" />
             </span>
-            <input type="text" placeholder="Add a task" />
+            <input
+              type="text"
+              placeholder="Add a task"
+              onChange={(e) => setTitle(e.target.value)}
+            />
           </div>
         </div>
         <div className="all-tasks">
-          <h2>Task</h2>
+          <h3>Task - {todos.length}</h3>
 
           <div className="tasks">
             {todos.map((todo) => (
-              <div key={todo.id} className="task">
-                <div className="left">
-                  <span>
-                    <BsCircle className="circle-icon" />
-                  </span>
-                  <div className="task-text">
-                    <h4>{todo.title}</h4>
-                    <span>0/1</span> &nbsp;&nbsp;
-                    <span>
-                      <MdOutlineCalendarToday /> Today
+              <div key={todo.id}>
+                <div className="task">
+                  <div className="left">
+                    <span onClick={() => markTodoAsComplete(todo.id)}>
+                      {todo.completed ? (
+                        <AiFillCheckCircle className="circle-icon" />
+                      ) : (
+                        <BsCircle className="circle-icon" />
+                      )}
                     </span>
+
+                    {/* <span>
+                      <input
+                        type="checkbox"
+                        checked={todo.completed}
+                        onChange={() => markTodoAsComplete(todo.id)}
+                      />
+                    </span> */}
+                    <div className="task-text">
+                      {edit === todo.id ? (
+                        <input
+                          type="text"
+                          className="edit-todo-input"
+                          value={title}
+                          onChange={(e) => setTitle(e.target.value)}
+                        />
+                      ) : (
+                        <h4>{todo.title}</h4>
+                      )}
+                      {edit === todo.id ? (
+                        ""
+                      ) : (
+                        <div>
+                          <span>0/1</span> &nbsp;&nbsp;
+                          <span>
+                            <MdOutlineCalendarToday /> Today
+                          </span>
+                        </div>
+                      )}
+                    </div>
                   </div>
+                  {edit === todo.id ? (
+                    <button
+                      className="edit-button"
+                      onClick={() => updateTodo(todo.id, title)}
+                    >
+                      Edit todo
+                    </button>
+                  ) : (
+                    <div className="todo-icons">
+                      <div className="todo-icons-inner">
+                        <span onClick={() => removeTodo(todo.id)}>
+                          <MdAdd />
+                        </span>
+                        <span
+                          onClick={() => handleEditChange(todo.id, todo.title)}
+                        >
+                          <MdEdit />
+                        </span>
+                        <span onClick={() => removeTodo(todo.id)}>
+                          <MdDelete />
+                        </span>
+                      </div>
+                      <span
+                        className="arrow"
+                        onClick={() => {
+                          if (todo.subTodos.length === 0) {
+                            alert("Add a todo");
+                            return;
+                          }
+
+                          setShowSubtodo(!showSubtodo);
+                        }}
+                      >
+                        {showSubtodo ? <BiChevronUp /> : <BiChevronDown />}
+                      </span>
+                    </div>
+                  )}
                 </div>
-                <span>
-                  <BiChevronDown />
-                </span>
+                {todo.subTodos &&
+                  todo.subTodos.map((subTodo) => (
+                    <div
+                      key={subTodo.id}
+                      className={showSubtodo ? "sub-todo" : "none"}
+                    >
+                      <div className="sub-todo-content">
+                        <span>
+                          {todo.completed ? (
+                            <AiFillCheckCircle className="circle-icon" />
+                          ) : (
+                            <BsCircle className="circle-icon" />
+                          )}
+                        </span>
+                        {/* <span>
+                          <input
+                            type="checkbox"
+                            checked={subTodo.completed}
+                            onChange={() => markTodoAsComplete(subTodo.id)}
+                          />
+                        </span> */}
+                        <div className="task-text">
+                          <h4>{subTodo.title}</h4>
+                        </div>
+                      </div>
+                    </div>
+                  ))}
               </div>
             ))}
           </div>
         </div>
+
+        {completedTodos.length !== 0 && (
+          <div className="completed-tasks">
+            <h3>Completed - {completedTodos.length}</h3>
+
+            {completedTodos.map((todo) => (
+              <div key={todo.id} className="completed-task">
+                <span onClick={() => markTodoAsComplete(todo.id)}>
+                  {todo.completed ? (
+                    <AiFillCheckCircle className="circle-icon" />
+                  ) : (
+                    <BsCircle className="circle-icon" />
+                  )}
+                </span>
+                {/* <span>
+                  <input
+                    type="checkbox"
+                    checked={todo.completed}
+                    onChange={() => markTodoAsComplete(todo.id)}
+                  />
+                </span> */}
+                <h4>{todo.title}</h4>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
